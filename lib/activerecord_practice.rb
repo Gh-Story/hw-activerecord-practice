0f45f625c4eaf37fd63d59f05eb97d6ccb83c29e
Gh-Story <hg820801350@icloud.com>
2021-10-16 18:22:38 -0700

    finished homework

--- lib/activerecord_practice.rb
+++ lib/activerecord_practice.rb
@@ -15,9 +15,56 @@ class Customer < ActiveRecord::Base
   def self.any_candice
     # YOUR CODE HERE to return all customer(s) whose first name is Candice
     # probably something like:  Customer.where(....)
+    Customer.where(first: 'Candice')
   end
+
   def self.with_valid_email
     # YOUR CODE HERE to return only customers with valid email addresses (containing '@')
+    Customer.where('email LIKE ?', '%@%').all
   end
-  # etc. - see README.md for more details
+
+  def self.with_dot_org_email
+    Customer.where('email LIKE ?', '%.org%').all
+  end
+
+  def self.with_invalid_email
+    Customer.where('email not LIKE ? AND email != ? AND email IS NOT NULL','%@%', '').all
+  end
+
+  def self.with_blank_email
+    Customer.where("email is null")
+  end
+
+  def self.born_before_1980
+    Customer.where('birthdate < ?', '1980-01-01')
+  end
+
+  def self.with_valid_email_and_born_before_1980
+    Customer.where('email Like ? AND birthdate < ?', '%@%', '1980-01-01')
+  end
+
+  def self.last_names_starting_with_b
+    Customer.where('last LIKE ?', 'B%').order(:birthdate)
+  end
+
+  def self.twenty_youngest
+    Customer.order(birthdate: :desc).limit(20)
+  end
+
+  def self.update_gussie_murray_birthdate
+    Customer.find_by('first = ? AND last = ?', 'Gussie', 'Murray').update(birthdate: Time.parse("2004-02-08"))
+  end
+
+  def self.change_all_invalid_emails_to_blank
+    Customer.where("email != '' AND email IS NOT NULL and email NOT LIKE '%@%'").update_all(email: '')
+  end
+
+  def self.delete_meggie_herman
+    Customer.find_by('first = ? AND last = ?', 'Meggie', 'Herman').destroy
+  end
+
+  def self.delete_everyone_born_before_1978
+    Customer.where('birthdate < ?', Time.parse("1 January 1978")).delete_all
+  end
+
 end
